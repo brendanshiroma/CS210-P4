0098246a45e7d3f2bb101be23d45342a04bcf3d5
brendanshiroma <brendan.shiroma31@gmail.com>
2024-04-21 20:59:30 -0700

    initial commit

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,5 +1,27 @@
 #include "HashTable.h"
 
 namespace HashTableNS {
+
+public:
+    HashTable() {
+
+    }
+
+    virtual ~HashTable() {
+
+    }
+
+    bool containsString(string target) const {
+
+    }
+
+    void put(string s) {
+
+    }
+
+private:
+    static int computeHash(string s) {
+
+    }
     
 } // namespace HashTableNS
--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,35 @@
 #include "LinkedList.h"
 
+
 namespace LinkedListNS {
+    template<class T>
+    
+    void headInsert(Node<T>*& head, const T& theData) {
+        Node<T> newNode = 
+
+    }
+
+    template<class T>
+    void insert(Node<T>* afterMe, const T& theData) {
+
+    }
+
+    template<class T>
+    void deleteNode(Node<T>* before) {
+
+    }
+
+    template<class T>
+    void deleteFirstNode(Node<T>*& head) {
+
+    }
+
+    template<class T>
+    Node<T>* search(Node<T>* head, const T& target) {
+
+    }
+
     
 } // namespace LinkedListNS
+
+

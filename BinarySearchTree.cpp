b7bb12e109219a79e06c65e01b2b4f234fc4d6e5
brendanshiroma <brendan.shiroma31@gmail.com>
2024-04-22 17:14:33 -0700

    started bst.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,4 +2,57 @@
 
 namespace BST_NS {
 
+    BinarySearchTree() {
+        root(NULL);
+        tree_size(0);
+    }
+
+    BinarySearchTree(const BinarySearchTree& other) {
+
+    }
+
+    virtual ~BinarySearchTree() {
+
+    }
+
+    void insert(T item) {
+
+    }
+
+    void remove(T item) {
+
+    }
+
+    bool inTree(T item) const {
+
+    }
+
+    BinarySearchTree& operator=(const BinarySearchTree other) {
+
+    }
+
+    void makeEmpty() {
+
+    }
+
+    void preOrderShow() const {
+
+    }
+
+    void inOrderShow() const {
+
+    }
+
+    void postOrderShow() const {
+
+    }
+
+    int size() const {
+
+    }
+
+    int height() {
+
+    }
+    
 } // namespace BST_NS

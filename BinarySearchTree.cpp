ae4cc5acb7e8fda295dd283394d46209d54ac2a0
brendanshiroma <brendan.shiroma31@gmail.com>
2024-04-28 00:03:54 -0700

    did a lot of things

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,62 +2,253 @@
 
 namespace BST_NS {
 
-    BinarySearchTree() {
-        root(NULL);
-        tree_size(0);
-    }
+    // Default Constructor
+    template<class T>
+    BinarySearchTree<T>::BinarySearchTree() : root(nullptr), tree_size(0) {}
 
-    BinarySearchTree(const BinarySearchTree& other) {
-        makeEmpty();
-        copyTree(other.root, root); //wrong fix this
+    // Copy Constructor
+    template<class T>
+    BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree& other) {
+        root = copyTree(other.root);
         tree_size = other.tree_size;
     }
 
-    virtual ~BinarySearchTree() {
-        
-    }
+    // Destructor
     template<class T>
-    void insert(T item) {
-        TreeNode<T> *newNode = new TreeNode(item);
-        if(newNode < ) {
+    BinarySearchTree<T>::~BinarySearchTree() {
+        makeEmpty();
+    }
 
+    // Insert an item into the tree
+    template<class T>
+    void BinarySearchTree<T>::insert(T item) {
+        if (root == nullptr) {
+            root = new TreeNode<T>(item, nullptr, nullptr);
+            tree_size++;
+        } else {
+            TreeNode<T>* curr = root;
+            while (true) {
+                if (item < curr->data) {
+                    if (curr->leftLink == nullptr) {
+                        curr->leftLink = new TreeNode<T>(item, nullptr, nullptr);
+                        tree_size++;
+                        break;
+                    } else {
+                        curr = curr->leftLink;
+                    }
+                } else {
+                    if (curr->rightLink == nullptr) {
+                        curr->rightLink = new TreeNode<T>(item, nullptr, nullptr);
+                        tree_size++;
+                        break;
+                    } else {
+                        curr = curr->rightLink;
+                    }
+                }
+            }
         }
     }
+
+    // Remove an item from the tree
     template<class T>
-    void remove(T item) {
-        
+    void BinarySearchTree<T>::remove(T item) {
+        if (root == nullptr)
+            return;
+
+        TreeNode<T>* parent = nullptr;
+        TreeNode<T>* curr = root;
+
+        while (curr != nullptr && curr->data != item) {
+            parent = curr;
+            if (item < curr->data)
+                curr = curr->leftLink;
+            else
+                curr = curr->rightLink;
+        }
+
+        if (curr == nullptr)
+            return;
+
+        // Case 1: Node has no children
+        if (curr->leftLink == nullptr && curr->rightLink == nullptr) {
+            if (parent == nullptr) {
+                delete root;
+                root = nullptr;
+            } else if (parent->leftLink == curr) {
+                delete parent->leftLink;
+                parent->leftLink = nullptr;
+            } else {
+                delete parent->rightLink;
+                parent->rightLink = nullptr;
+            }
+            tree_size--;
+        }
+        // Case 2: Node has one child
+        else if (curr->leftLink == nullptr) {
+            if (parent == nullptr) {
+                root = curr->rightLink;
+            } else if (parent->leftLink == curr) {
+                parent->leftLink = curr->rightLink;
+            } else {
+                parent->rightLink = curr->rightLink;
+            }
+            delete curr;
+            tree_size--;
+        } else if (curr->rightLink == nullptr) {
+            if (parent == nullptr) {
+                root = curr->leftLink;
+            } else if (parent->leftLink == curr) {
+                parent->leftLink = curr->leftLink;
+            } else {
+                parent->rightLink = curr->leftLink;
+            }
+            delete curr;
+            tree_size--;
+        }
+        // Case 3: Node has two children
+        else {
+            TreeNode<T>* successor = curr->rightLink;
+            TreeNode<T>* successorParent = curr;
+
+            while (successor->leftLink != nullptr) {
+                successorParent = successor;
+                successor = successor->leftLink;
+            }
+
+            if (successorParent != curr) {
+                successorParent->leftLink = successor->rightLink;
+            } else {
+                successorParent->rightLink = successor->rightLink;
+            }
+
+            curr->data = successor->data;
+            delete successor;
+            tree_size--;
+        }
     }
+
+    // Check if an item exists in the tree
     template<class T>
-    bool inTree(T item) const {
-
+    bool BinarySearchTree<T>::inTree(T item) const {
+        TreeNode<T>* curr = root;
+        while (curr != nullptr) {
+            if (curr->data == item) {
+                return true;
+            } else if (item < curr->data) {
+                curr = curr->leftLink;
+            } else {
+                curr = curr->rightLink;
+            }
+        }
+        return false;
     }
 
-    BinarySearchTree& operator=(const BinarySearchTree other) {
-
+    // Assignment operator overload
+    template<class T>
+    BinarySearchTree<T>& BinarySearchTree<T>::operator=(const BinarySearchTree other) {
+        if (this != &other) {
+            makeEmpty();
+            root = copyTree(other.root);
+            tree_size = other.tree_size;
+        }
+        return *this;
     }
 
-    void makeEmpty() {
-
+    // Empty the tree
+    template<class T>
+    void BinarySearchTree<T>::makeEmpty() {
+        deleteTreeHelper(root);
+        root = nullptr;
+        tree_size = 0;
     }
 
-    void preOrderShow() const {
-
+    // Pre-Order Traversal
+    template<class T>
+    void BinarySearchTree<T>::preOrderShow() const {
+        preOrderHelper(root);
     }
 
-    void inOrderShow() const {
-
+    // In-Order Traversal
+    template<class T>
+    void BinarySearchTree<T>::inOrderShow() const {
+        inOrderHelper(root);
     }
 
-    void postOrderShow() const {
-
+    // Post-Order Traversal
+    template<class T>
+    void BinarySearchTree<T>::postOrderShow() const {
+        postOrderHelper(root);
     }
 
-    int size() const {
-
+    // Return the size of the tree
+    template<class T>
+    int BinarySearchTree<T>::size() const {
+        return tree_size;
     }
 
-    int height() {
-
+    // Calculate the height of the tree
+    template<class T>
+    int BinarySearchTree<T>::height() {
+        return treeHeight(root);
     }
-    
+
+    // Helper function to copy a tree
+    template<class T>
+    TreeNode<T>* copyTree(TreeNode<T>* node) {
+        if (node == nullptr)
+            return nullptr;
+        else
+            return new TreeNode<T>(node->data, copyTree(node->leftLink), copyTree(node->rightLink));
+    }
+
+    // Helper function to delete a tree
+    template<class T>
+    void deleteTreeHelper(TreeNode<T>* &node) {
+        if (node != nullptr) {
+            deleteTreeHelper(node->leftLink);
+            deleteTreeHelper(node->rightLink);
+            delete node;
+            node = nullptr;
+        }
+    }
+
+    // Helper function for pre-order traversal
+    template<class T>
+    void preOrderHelperHelper(TreeNode<T>* node) const {
+        if (node != nullptr) {
+            std::cout << node->data << " ";
+            preOrderHelper(node->leftLink);
+            preOrderHelper(node->rightLink);
+        }
+    }
+
+    // Helper function for in-order traversal
+    template<class T>
+    void inOrderHelper(TreeNode<T>* node) const {
+        if (node != nullptr) {
+            inOrderHelper(node->leftLink);
+            std::cout << node->data << " ";
+            inOrderHelper(node->rightLink);
+        }
+    }
+
+    // Helper function for post-order traversal
+    template<class T>
+    void postOrderHelper(TreeNode<T>* node) const {
+        if (node != nullptr) {
+            postOrderHelper(node->leftLink);
+            postOrderHelper(node->rightLink);
+            std::cout << node->data << " ";
+        }
+    }
+
+    // Helper function to calculate the height of the tree
+    template<class T>
+    int treeHeight(TreeNode<T>* node) {
+        if (node == nullptr)
+            return -1;
+        else
+            return 1 + std::max(treeHeight(node->leftLink), treeHeight(node->rightLink));
+    }
+
 } // namespace BST_NS
--- HashTable.cpp
+++ HashTable.cpp
@@ -2,26 +2,49 @@
 
 namespace HashTableNS {
 
-public:
-    HashTable() {
-
+    // Default Constructor
+    HashTable::HashTable() {
+        // Initialize hashArray to nullptr
+        for (int i = 0; i < SIZE; ++i) {
+            hashArray[i] = nullptr;
+        }
     }
 
-    virtual ~HashTable() {
-
+    // Destructor
+    HashTable::~HashTable() {
+        // Delete all linked lists in hashArray
+        for (int i = 0; i < SIZE; ++i) {
+            delete hashArray[i];
+        }
     }
 
-    bool containsString(string target) const {
-
+    // Checks if the target is in the hash table
+    bool HashTable::containsString(string target) const {
+        int hashValue = computeHash(target);
+        if (hashArray[hashValue] == nullptr) {
+            return false;
+        } else {
+            return hashArray[hashValue]->contains(target);
+        }
     }
 
-    void put(string s) {
-
+    // Adds a new string to the hash table
+    void HashTable::put(string s) {
+        int hashValue = computeHash(s);
+        if (hashArray[hashValue] == nullptr) {
+            hashArray[hashValue] = new Node<string>(s, nullptr);
+        } else {
+            hashArray[hashValue]->insert(s);
+        }
     }
 
-private:
-    static int computeHash(string s) {
-
+    // Compute hash value for string
+    int HashTable::computeHash(string s) {
+        int hash = 0;
+        for (char c : s) {
+            hash += c;
+        }
+        return hash % SIZE;
     }
-    
+
 } // namespace HashTableNS
